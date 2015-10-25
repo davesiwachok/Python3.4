ef4f45fc50fdf2e14a871fd3c0fe5f408a7e7c07
davesiwachok <dave@handymanchicago.net>
2015-10-25 07:06:42 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,14 @@
 import http.server
 import socketserver
 import datetime
-
 PORT = 8082
-
 Handler = http.server.SimpleHTTPRequestHandler
-
 httpd = socketserver.TCPServer(("", PORT), Handler)
-
 print("Server running on port", PORT)
 today = datetime.date.today()
 print (today.strftime("%m/%d/%Y"))  
-
 httpd.serve_forever()
 # Starts server
 # http://127.0.0.1:8082
 # Uses index.html file in the directory that this file is in.
+#Prints date to shell
